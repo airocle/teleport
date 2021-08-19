ec76e52039deb91d3b0b17b1c5ed1c2750894d90
Alan Parra <alan.parra@goteleport.com>
2021-08-19 18:33:56 -0300

    Login test cleanup

--- lib/auth/webauthn/login_test.go
+++ lib/auth/webauthn/login_test.go
@@ -8,22 +8,23 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/auth/mocku2f"
 	"github.com/gravitational/teleport/lib/auth/webauthn"
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	initialCounter = 10
-)
+const currentCounter = 10
 
-func TestLoginFlow_LoginSuccessful_u2f(t *testing.T) {
-	// Let's simulate a pre-existing U2F device.
+func TestLoginFlow_BeginFinish_u2f(t *testing.T) {
+	// Let's simulate a previously registered U2F device, without going through
+	// the trouble of actually registering it.
 	dev, err := mocku2f.Create()
 	require.NoError(t, err)
-	dev.SetCounter(initialCounter)
-	mfaDev, err := keyToMFADevice(dev, initialCounter)
+	dev.SetCounter(currentCounter)
+	devAddedAt := time.Now().Add(-5 * time.Minute) // Make sure devAddedAt is in the past.
+	mfaDev, err := keyToMFADevice(dev, currentCounter-1, devAddedAt /* addedAt */, devAddedAt /* lastUsed */)
 	require.NoError(t, err)
 
 	user := &types.UserV2{
@@ -41,36 +42,48 @@ func TestLoginFlow_LoginSuccessful_u2f(t *testing.T) {
 		SessionData: make(map[string]*webauthn.SessionData),
 	}
 
+	u2fConfig := types.U2F{AppID: "https://example.com:3080"}
+	webConfig := webauthn.Config{RPID: "example.com"}
 	webLogin := webauthn.LoginFlow{
-		U2F:      &types.U2F{AppID: "https://example.com:3080"},
-		Webauthn: &webauthn.Config{RPID: "example.com"},
+		U2F:      &u2fConfig,
+		Webauthn: &webConfig,
 		Identity: identity,
 	}
 
 	ctx := context.Background()
+
+	// webLogin.Begin and webLogin.Finish are the actual methods under test, the
+	// rest is setup/sanity checking.
 	assertion, err := webLogin.Begin(ctx, user.GetName())
 	require.NoError(t, err)
-
-	// TODO(codingllama): Assert assertion.
+	// We care about RPID and AppID, for everything else defaults are OK.
+	require.Equal(t, webConfig.RPID, assertion.Response.RelyingPartyID)
+	require.Equal(t, u2fConfig.AppID, assertion.Response.Extensions["appid"])
 
 	assertionResp, err := dev.SignAssertion("https://example.com:3080" /* origin */, assertion)
 	require.NoError(t, err)
 
+	// webLogin.Finish is the other part of the test, completing the login flow.
+	beforeLastUsed := time.Now().Add(-1 * time.Second)
 	loginDevice, err := webLogin.Finish(ctx, user.GetName(), assertionResp)
 	require.NoError(t, err)
-	require.NotNil(t, loginDevice)
+	require.True(t, beforeLastUsed.Before(loginDevice.LastUsed))
 
-	// TODO(codingllama): Assert login device.
+	// Last used time and counter are be updated, everything else is equal.
+	wantDev, _ := keyToMFADevice(dev, currentCounter, devAddedAt, loginDevice.LastUsed)
+	if diff := cmp.Diff(wantDev, loginDevice); diff != "" {
+		t.Errorf("Finish() mismatch (-want +got):\n%s", diff)
+	}
 }
 
-func keyToMFADevice(dev *mocku2f.Key, counter uint32) (*types.MFADevice, error) {
+func keyToMFADevice(dev *mocku2f.Key, counter uint32, addedAt, lastUsed time.Time) (*types.MFADevice, error) {
 	pubKeyDER, err := x509.MarshalPKIXPublicKey(&dev.PrivateKey.PublicKey)
 	if err != nil {
 		return nil, err
 	}
 	return &types.MFADevice{
-		AddedAt:  time.Now().Add(5 * time.Minute),
-		LastUsed: time.Now().Add(5 * time.Minute),
+		AddedAt:  addedAt,
+		LastUsed: lastUsed,
 		Device: &types.MFADevice_U2F{
 			U2F: &types.U2FDevice{
 				KeyHandle: dev.KeyHandle,
